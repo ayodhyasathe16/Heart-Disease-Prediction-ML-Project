8e522e285de9bfb96b78e8f9aa53f542626e1037
Ayodhya Mohan Sathe <93192770+ayodhyasathe16@users.noreply.github.com>
2021-11-23 19:06:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import pickle
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from heart_predict import show_predict_page
+from explore_page import show_explore_page
+
+
+df = pd.read_csv('heart.csv')
+
+x = df.drop(['target'], axis=1)
+y = df['target']
+
+
+sex_dictionary = {'Male':1, 'Female':0}
+fasting_dictionary = {'true':1, 'false':0}
+ex_dict = {'yes':1, 'no':0}
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
